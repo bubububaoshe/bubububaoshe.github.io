acf292c6e01997b09b075f985516c3057060e569
nitroglycerine <suichen6@gmail.com>
2019-01-17 03:21:13 -0800

    testing inviting player by id

--- index.js
+++ index.js
@@ -60,9 +60,16 @@ class PlayerMatcher {
 			var p0 = this.match_queue.pop();
 			var p1 = this.match_queue.pop();
 			this.matched_pairs.push([p0, p1]);
-			p0.emit("Match_FoundMatch");
-			p1.emit("Match_FoundMatch");
+			p0.emit("Match_FoundMatch", "已找到对手，请确认：");
+			p1.emit("Match_FoundMatch", "已找到对手，请确认：");
 		}
+	}
+	
+	// P0 找 P1
+	BringTwoPlayersIntoPair(p0, p1) {
+	  this.matched_pairs.push([p0, p1]);
+	  p0.emit("Match_FoundMatch", "找到了这位小伙伴，请等待ta确认：");
+	  p1.emit("Match_FoundMatch", "玩家"+p0.player_id+"邀请你来一局，请确认是否开始：");
 	}
 
 	FindPairByPlayer(p) {
@@ -107,12 +114,28 @@ class PlayerMatcher {
 		}
 	}
 	
+	OnPlayerCancelsMatch(p) {
+	  var pair = this.FindPairByPlayer(p);
+	  this.match_queue.remove(p);
+	  if (pair != null) {
+	    var other = null;
+	    if (pair[0] == p) other = pair[1]; else other = pair[0];
+	    other.emit('Match_OtherPlayerCancelsMatch');
+	  }
+	}
+	
 	StartGame(pair) {
 	  console.log("Should start a game now");
 	  this.matched_pairs.RemovePair(pair);
 	  var g = new Game(pair[0], pair[1]);
 	  g_all_games.push(g);
 	  g.Start();
+	}
+	
+	IsPlayerInMatchQueueOrMatched(p) {
+	  if (p in this.match_queue) return true;
+	  else if (this.FindPairByPlayer(p) != null) return true;
+	  else return false;
 	}
 }
 
@@ -233,6 +256,16 @@ class Game {
       other_socket.emit('Game_OppoRedeal', pool_cids);
     }
   }
+  
+  OnPlayerDisconnectedOrCanceled(p) {
+    var pidx = this.GetPlayerIndexBySocket(p);
+    console.log('[OnPlayerDisconnectedOrCanceled] player_id='+p.player_id);
+    if (pidx != -1) {
+      g_all_games.remove(this);
+      var other_socket = this.players[1-pidx];
+      other_socket.emit('Room_PlayerDisconnected');
+    }
+  }
 };
 
 FindGameBySocket = function(socket) {
@@ -246,8 +279,10 @@ io.on('connection', function(socket) {
 	socket.match_confirmed = false;
 	g_serial += 1;
 	
-	var bcast = true;
 	socket.emit('Info_OnlinePlayerCount', g_all_sockets.length);
+	socket.emit('Info_MyPlayerId', socket.player_id);
+	
+	var bcast = true;
 	if (bcast) socket.broadcast.emit('Info_OnlinePlayerCount', g_all_sockets.length);
 	
 	console.log("player " + socket.player_id + " joined, " +
@@ -255,10 +290,13 @@ io.on('connection', function(socket) {
 
 	// 离线
 	socket.on('disconnect', () => {
+	  // 若是没开始...
 		g_playermatcher.OnPlayerCancelsMatch(socket);
 		g_all_sockets.remove(socket);
+		// 若是已开始了...
 		var g = FindGameBySocket(socket);
-	  g_all_games.remove(g);
+		if (g != undefined)
+		  g.OnPlayerDisconnectedOrCanceled(socket);
 		console.log("player " + socket.player_id + " left, " +
 		  g_all_sockets.length + " sockets, " + g_all_games.length + " games");
 	});
@@ -266,6 +304,10 @@ io.on('connection', function(socket) {
 	socket.on('Match_ReadyToMatch', () => {
 		g_playermatcher.OnNewPlayerReadyToMatch(socket);
 		g_playermatcher.MatchOnePair();
+	});
+	
+	socket.on('Match_CancelMatch', () => {
+	  g_playermatcher.OnPlayerCancelsMatch(socket);
 	});
 	
 	socket.on('Match_ConfirmMatch', () => {
@@ -311,6 +353,34 @@ io.on('connection', function(socket) {
 	
 	socket.on('Info_RefreshOnlinePlayerCount', () => {
 	  socket.emit('Info_OnlinePlayerCount', g_all_sockets.length);
+	});
+	
+	// socket invites another player with iid
+	socket.on('Match_InvitePlayer', (iid) => {
+	  if (iid == socket.player_id) {
+	    socket.emit('Match_InvalidInvitation', '不能邀请自己');
+	  } else {
+	    var ok = false;
+      for (var i=0; i<g_all_sockets.length; i++) {
+        var p = g_all_sockets[i];
+        console.log('[Invite] checking ' + p.player_id);
+        if (p.player_id == iid) {
+          if (g_playermatcher.IsPlayerInMatchQueueOrMatched(p)) {
+            socket.emit('Match_InvalidInvitation', '对方正在随机匹配队列中');
+          } else if (FindGameBySocket(p) != null) {
+            socket.emit('Match_InvalidInvitation', '对方正在游戏中');
+          } else {
+            // Put both players into the matching queue and match them
+            g_playermatcher.BringTwoPlayersIntoPair(socket, p);
+            ok = true;
+            break;
+          }
+        }
+      }
+      if (ok == false) {
+        socket.emit('Match_InvalidInvitation', '未找到该ID对应的玩家');
+      }
+    }
 	});
 });
 
